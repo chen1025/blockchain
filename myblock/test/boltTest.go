ebef801de92902a341968c0848554797fbd469bb
chen <1025904790@qq.com>
2020-08-28 11:39:40 +0800

    修改成数据库存储

---
+++ myblock/test/boltTest.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"blockchain/bolt"
+	"fmt"
+)
+
+func main() {
+
+	// 打开数据库
+	open, err := bolt.Open("test.db", 0600, nil)
+	if err != nil {
+		panic(err)
+	}
+	// 关闭数据库
+	defer open.Close()
+	// 新增数据
+	open.Update(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte("v1"))
+		if bucket == nil {
+			bucket, err = tx.CreateBucket([]byte("v1"))
+			if err != nil {
+				panic(err)
+			}
+		}
+		bucket.Put([]byte("god"), []byte("first"))
+		bucket.Put([]byte("user"), []byte("second"))
+		return nil
+	})
+	// 获取 数据
+	open.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte("v1"))
+		god := bucket.Get([]byte("god"))
+		user := bucket.Get([]byte("user"))
+		fmt.Println(god)
+		fmt.Println(user)
+		return nil
+	})
+
+}
